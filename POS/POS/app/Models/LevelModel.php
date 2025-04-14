2fa418748791ea76c28620646e72bcdc403cbefb
renaldAgustinus <renaldagustinus@gmail.com>
2025-04-14 13:21:53 +0700

    Implementasi Layout adminlte

--- POS/POS/app/Models/LevelModel.php
+++ POS/POS/app/Models/LevelModel.php
@@ -4,19 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use App\Models\UserModel;
 
 class LevelModel extends Model
 {
     use HasFactory;
+
     protected $table = 'm_level';
     protected $primaryKey = 'level_id';
-
-    public function user(): HasMany
-    {
-        return $this->hasMany(UserModel::class, 'level_id', 'level_id');
-    }
+    protected $guarded = [];
 }
